d43610f190e2f386965ecf63d8dd92a97f7187c5
Kicasz5 <r90002@gmail.com>
2016-01-30 15:41:25 -0800

    IOS Switch

---
+++ Parket/Parket/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Parket
+//
+//  Created by Ray Yang on 1/30/16.
+//  Copyright © 2016 CSE110W240T16. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let camera = GMSCameraPosition.cameraWithLatitude(-33.86,
+            longitude: 151.20, zoom: 6)
+        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        mapView.myLocationEnabled = true
+        self.view = mapView
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+    }
+}
