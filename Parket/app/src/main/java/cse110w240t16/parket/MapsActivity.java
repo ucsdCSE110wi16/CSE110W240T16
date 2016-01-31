79beb45fc4708be0b78f4f5efcb0785630d314cb
Kicasz5 <r90002@gmail.com>
2016-01-30 22:52:13 -0800

    Search bar

--- Parket/app/src/main/java/cse110w240t16/parket/MapsActivity.java
+++ Parket/app/src/main/java/cse110w240t16/parket/MapsActivity.java
@@ -1,7 +1,9 @@
 package cse110w240t16.parket;
 
+import android.content.pm.PackageManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,6 +11,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.jar.Manifest;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -42,5 +46,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+//        mMap.setMyLocationEnabled(true);
     }
 }
