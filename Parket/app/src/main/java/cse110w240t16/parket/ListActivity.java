b1f69e0fd9c03951f216cbf415721bddc26b164e
YChH <hayuanchi@gmail.com>
2016-02-04 13:28:49 -0800

    another button

--- Parket/app/src/main/java/cse110w240t16/parket/ListActivity.java
+++ Parket/app/src/main/java/cse110w240t16/parket/ListActivity.java
@@ -27,3 +27,60 @@ public class ListActivity extends Activity{
         });
     }
 }
+
+
+/**
+public class ListActivity extends AppCompatActivity{
+
+    private ArrayList<String> data = new ArrayList<String>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        ListView lv = (ListView) findViewById(R.id.list)
+    }
+
+    private class MyListAdapter extends ArrayAdapter<string> {
+        private int layout;
+        public MyListAdapter(Context context, int resource, List<string> objects) {
+            super(context, resource, objects);
+            layout = resource;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder mainViewholder = null;
+            if(convertView == null){
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(layout, parent, false);
+                ViewHolder viewHolder = new ViewHolder();
+                //viewHolder.thumbnail = (ImageView) convertView.findViewById(R.id.)
+                viewHolder.button = (Button) convertView.findViewById(R.id.show);
+                viewHolder.button.setOnclickListener(new View.OnClickListener(){
+
+                    @Override
+                    public void onClick(View v) {
+                        //Toast.makeText(getContent(),)
+
+                    }
+                });
+                convertView.setTag(viewHolder);
+            }
+            else{
+                mainViewholder = (ViewHolder) convertView.getTag();
+                //mainViewholder.title.setText(getItem(position));
+            }
+            return convertView;
+
+        }
+    }
+
+    public class ViewHolder {
+        //ImageView thumbnail;
+        //TextView title;
+        Button button;
+
+    }
+}
+**/
