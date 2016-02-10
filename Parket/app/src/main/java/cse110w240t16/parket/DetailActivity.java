1cfcc261107fa56bf61fca159b04caff35772093
XiaoyuePu <XiaoyuePu@Xiaoyues-MacBook-Pro.local>
2016-02-10 14:28:04 -0800

    back activity modification

--- Parket/app/src/main/java/cse110w240t16/parket/DetailActivity.java
+++ Parket/app/src/main/java/cse110w240t16/parket/DetailActivity.java
@@ -3,8 +3,6 @@ package cse110w240t16.parket;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 /**
  * Created by XiaoyuePu on 2/4/16.
@@ -15,18 +13,14 @@ public class DetailActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail_view);
 
-        Button goBackBtn;
-        goBackBtn = (Button) findViewById(R.id.returnBtn);
-        goBackBtn.setOnClickListener(new View.OnClickListener() {
 
-            public void onClick(View v) {
-                Intent myIntent = new Intent(v.getContext(), ListActivity.class);
-                startActivityForResult(myIntent, 0);
-                //Intent result = new Intent();
-                //setResult(RESULT_OK, result);
-                finish();
-            }
-        });
+    }
+    @Override
+    public void onBackPressed() {
+
+        Intent intent = new Intent(this, ListActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
 
